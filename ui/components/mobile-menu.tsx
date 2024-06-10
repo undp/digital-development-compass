ded0cddd103f2e34ee9603f4637e0a1b28a218c5
Janith Rathnayaka <janithr@xepatagon.com>
2024-06-10 14:19:39 +0530

    mobile ui page

--- ui/components/mobile-menu.tsx
+++ ui/components/mobile-menu.tsx
@@ -1,44 +1,34 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import Link from "next/link";
 
-import { SearchDialogInner } from "./search-dialog";
-
-export function MobileMenu({
-  isOpen,
-  onClose,
-  countries,
-}: {
+// Define the interface for the props
+interface MobileMenuProps {
   isOpen: boolean;
   onClose: () => void;
   countries: CountryNameAndAlpha[];
-}) {
+}
+
+export function MobileMenu({ isOpen, onClose, countries }: MobileMenuProps) {
   return (
     <Dialog.Root open={isOpen} onOpenChange={onClose}>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 bg-black/30 backdrop-blur-sm" />
-        <Dialog.Content className="fixed bg-white inset-0 right-0 mx-3 top-28 rounded-tl-xl rounded-tr-xl overflow-hidden z-10">
-          <Dialog.Title className="sr-only">Mobile Navigation</Dialog.Title>
-          <SearchDialogInner
-            hideRecent
-            onClose={() => {}}
-            countries={countries}
-          />
-          <div className="p-4">
-            <p className="text-xs uppercase tracking-wide font-medium text-gray-600 block mb-2">
-              Navigation
-            </p>
-            <nav className="flex flex-col mt-2 divide-y">
-              <Link href="/about">
-                <a className="text-1xl py-2">About</a>
-              </Link>
-              <Link href="/data">
-                <a className="text-1xl py-2">Data</a>
-              </Link>
-              <Link href="/methodology">
-                <a className="text-1xl py-2">Methodology</a>
-              </Link>
-            </nav>
-          </div>
+        <Dialog.Content
+          className={`fixed top-20 left-0 h-full w-full max-w-sm custom-mobile-bg-color bg-opacity-90 z-50 p-6 space-y-4 shadow-lg transform transition-transform duration-300 translate-x-0 ${
+            isOpen ? "custom-mobile-menu" : "custom-mobile-close-menu"
+          }`}
+          style={{ top: "7rem" }}
+        >
+          <nav className="flex flex-col space-y-4">
+            <Link href="/about">
+              <a className="uppercase text-lg hover:underline">About</a>
+            </Link>
+            <Link href="/data">
+              <a className="uppercase text-lg hover:underline">data</a>
+            </Link>
+            <Link href="/methodology">
+              <a className="uppercase text-lg hover:underline">Methodology</a>
+            </Link>
+          </nav>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
