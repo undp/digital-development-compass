dd814c03e240d6a868c34b42bbd332082b013fef
Janith Rathnayaka <janithr@xepatagon.com>
2024-06-11 18:02:46 +0530

    ui\components\mobile-menu.tsx

--- ui/components/mobile-menu.tsx
+++ ui/components/mobile-menu.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 // Define the interface for the props
 interface MobileMenuProps {
@@ -9,24 +10,81 @@ interface MobileMenuProps {
 }
 
 export function MobileMenu({ isOpen, onClose, countries }: MobileMenuProps) {
+  const [activeLink, setActiveLink] = useState<string | null>(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 994 && isOpen) {
+        onClose();
+      }
+    };
+
+    // Attach the resize event listener
+    window.addEventListener("resize", handleResize);
+
+    // Check once on initial render
+    handleResize();
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isOpen, onClose]);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [isOpen]);
+
+  const handleLinkClick = (link: string) => {
+    setActiveLink(link);
+  };
+
   return (
     <Dialog.Root open={isOpen} onOpenChange={onClose}>
       <Dialog.Portal>
         <Dialog.Content
-          className={`fixed top-20 left-0 h-full w-full max-w-sm custom-mobile-bg-color bg-opacity-90 z-50 p-6 space-y-4 shadow-lg transform transition-transform duration-300 translate-x-0 ${
+          className={`fixed top-20 left-0 h-full w-full custom-mobile-bg-color bg-opacity-90 z-50 space-y-4 shadow-lg transform transition-transform duration-300 translate-x-0 ${
             isOpen ? "custom-mobile-menu" : "custom-mobile-close-menu"
           }`}
-          style={{ top: "7rem" }}
+          style={{ top: "6rem" }}
         >
-          <nav className="flex flex-col space-y-4">
+          <nav className="flex flex-col w-full justify-center">
             <Link href="/about">
-              <a className="uppercase text-lg hover:underline">About</a>
+              <a
+                className={`uppercase text-lg w-full border-b p-6 h-20 border-white flex items-center ${
+                  activeLink === "/about" ? "footer-background-color text-white" : ""
+                }`}
+                onClick={() => handleLinkClick("/about")}
+              >
+                About
+              </a>
             </Link>
             <Link href="/data">
-              <a className="uppercase text-lg hover:underline">data</a>
+              <a
+                className={`uppercase text-lg w-full border-b h-20 border-white flex items-center p-6 ${
+                  activeLink === "/data" ? "footer-background-color text-white" : ""
+                }`}
+                onClick={() => handleLinkClick("/data")}
+              >
+                Data
+              </a>
             </Link>
             <Link href="/methodology">
-              <a className="uppercase text-lg hover:underline">Methodology</a>
+              <a
+                className={`uppercase text-lg w-full border-b h-20 border-white flex items-center p-6 ${
+                  activeLink === "/methodology" ? "footer-background-color text-white" : ""
+                }`}
+                onClick={() => handleLinkClick("/methodology")}
+              >
+                Methodology
+              </a>
             </Link>
           </nav>
         </Dialog.Content>
