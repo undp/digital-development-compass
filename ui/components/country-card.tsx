60e1c0f5e40d899b7af7921c16322c14503cdd92
Janith Rathnayaka <janithr@xepatagon.com>
2024-07-18 13:46:03 +0530

    update country-card

--- ui/components/country-card.tsx
+++ ui/components/country-card.tsx
@@ -72,7 +72,7 @@ export function CountryCard({
       {showFooterLink && (
         <div className="mb-4">
           <Link href={`/country/${country.alpha3}`}>
-            <a className="button-bg-color  hover:bg-brand-blue-dark text-xs uppercase font-semibold px-4 py-2 text-white flex-shrink-0 flex items-center">
+            <a className="bg-brand-blue-dark hover:button-bg-color text-xs uppercase font-semibold px-4 py-2 text-white flex-shrink-0 flex items-center">
               view more
             </a>
           </Link>
