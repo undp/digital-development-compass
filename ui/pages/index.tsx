d02544bb1cd82a0c70a788b3f0fe49a8a80297d9
Janith Rathnayaka <janithr@xepatagon.com>
2024-09-24 00:29:48 +0530

    fixed ui issues footer and header and about us

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -23,7 +23,7 @@ function Hero() {
       <h1 className="text-3xl md:hero-title-size font-bold">
         Is your nation ready to navigate digital transformation?
       </h1>
-      <p className="hero-content-text-size leading-7 mt-4 hero-content-text-color text-justify">
+      <p className="hero-content-text-size leading-7 mt-4 text-justify" style={{color: '#333333'}}>
       Explore your nation's digital progress using the world's largest database of digital development data. Leveraging the pillars of UNDP’s Digital Transformation Framework, the Digital Development Compass lets you discover and compare progress across a range of key issues.
       </p>
     </div>
@@ -57,7 +57,7 @@ const IndexPage = ({
       <div className="w-full lg:divide-x grid lg:grid-cols-12 h-full">
       <PopupMessage/>
       <div className="items-center justify-center px-6 py-6 lg:px-12 xl:px-16 col-span-full lg:col-span-5">
-            <div className="max-w-2xl pt-0 flex flex-col justify-center h-full md:mx-auto">
+            <div className="max-w-2xl flex flex-col justify-center h-full md:mx-auto pt-14">
               <Hero />
               <div className="mt-8 mb-4">
                 <PillarRadioGroup
