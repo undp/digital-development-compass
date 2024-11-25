5058d81b846bb60becb24415a3cd45b93870a9a6
Janith Rathnayaka <janithr@xeptaon.com>
2024-11-25 10:38:40 +0530

    update base path

--- ui/next.config.js
+++ ui/next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  basePath: "/digital-development-compass",
-  assetPrefix: "/digital-development-compass",
+  basePath: "",
+  assetPrefix: "",
   env: {
     PASSWORD_PROTECT: process.env.NODE_ENV !== "development",
     SITE_CONFIG: process.env.ENV ?? "staging"
