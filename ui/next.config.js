da9be5db500c3687715f351ead0a4e1f115915f1
Janith Rathnayaka <janithr@xeptaon.com>
2024-12-16 16:43:10 +0530

    update site config staging

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const SITE_CONFIG = process.env.ENV ?? "dev"; // Change this to "staging / local" as needed
+const SITE_CONFIG = process.env.ENV ?? "staging"; // Change this to "staging / local" as needed
 
 const nextConfig = {
   reactStrictMode: true,
