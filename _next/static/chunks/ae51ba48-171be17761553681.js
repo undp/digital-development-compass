3ef0b77d913fffe31400f61f32a442990c5832da
palindaa <99331873+palindaa@users.noreply.github.com>
2026-01-22 07:08:30 +0000

    Deploying to gh-pages from @ undp/digital-development-compass@a11db0e4aaf9c7ec477283d63a7b65c8fa5686bf 🚀

---
+++ _next/static/chunks/ae51ba48-171be17761553681.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[260],{60155:function(t,a,n){n.d(a,{bjh:function(){return e}});var u=n(88357);function e(t){return(0,u.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M289.94 256l95-95A24 24 0 00351 127l-95 95-95-95a24 24 0 00-34 34l95 95-95 95a24 24 0 1034 34l95-95 95 95a24 24 0 0034-34z"}}]})(t)}}}]);
